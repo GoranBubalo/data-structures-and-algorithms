38c874756cdf216b30049b26dde89c9e440cf3a7
goran <goranbubalo2422003@gmail.com>
2025-07-29 20:46:15 +0200

    updated the selection sort algorithm

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 //Selection sort algorithm
 int main() {
-    int arr[] = {54,2,76,33,0,2,1,85};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {54,2,76,33,0,2,1,85}; //8 elements
+    int n = sizeof(arr) / sizeof(arr[0]); // --> find the size of the array which is 8
 
-    for(int i = 0; i < n - 1; i++) {
+    for(int i = 0; i < n - 1; i++) { 
         int minIndex = i; 
 
         for(int j = i + 1; j < n; j++) {
@@ -14,7 +14,14 @@ int main() {
                 minIndex = j; // Update minIndex if a smaller element is found
             }
         }
+
+         //swap elements 
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
+
+   
 
     for(int num: arr) {
         cout << num << " "; // Print the sorted array
