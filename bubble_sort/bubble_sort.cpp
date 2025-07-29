23f18002fabb5d33b4011addd8f3e3c3a4afcc2f
goran <goranbubalo2422003@gmail.com>
2025-07-29 13:54:55 +0200

    Updated the bubble sort algoritham

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -4,12 +4,23 @@ using namespace std;
 
 // Learning bubble sort algorithm
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr[] = {4,6,2,7,22,1,3,5,8,9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
-    for(int i = 0; i < n ; i++){
-        cout << arr[i] << " ";
+
+    for(int i = 0; i < n - 1; i++) {
+        for( int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for(int num : arr) {
+        cout << num << " ";
     }
 
     cout << endl;
+    return 0;
 }
