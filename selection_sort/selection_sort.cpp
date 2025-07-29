c6e4d386c38b8872cb2b4a97a352e40b8c76cc01
goran <goranbubalo2422003@gmail.com>
2025-07-29 21:05:45 +0200

    refactor

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for(int i = 0; i < n - 1; i++) { 
         int minIndex = i; 
-
+        
         for(int j = i + 1; j < n; j++) {
             if(arr[j] < arr[minIndex]) {
                 minIndex = j; // Update minIndex if a smaller element is found
